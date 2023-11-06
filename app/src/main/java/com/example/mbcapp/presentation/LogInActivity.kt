f5e67438ed15fa453409c55cd8a827577464a01a
martin.delbel <martin.delbel@geopagos.com>
2023-11-06 18:10:53 -0300

    add log in button logic

--- app/src/main/java/com/example/mbcapp/presentation/LogInActivity.kt
+++ app/src/main/java/com/example/mbcapp/presentation/LogInActivity.kt
@@ -1,12 +1,49 @@
 package com.example.mbcapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mbcapp.R
+import androidx.activity.viewModels
+import com.example.mbcapp.databinding.ActivityLogInBinding
+import com.example.mbcapp.viewmodels.LogInState
+import com.example.mbcapp.viewmodels.LogInViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LogInActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityLogInBinding
+    private val viewModel: LogInViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        observeViewModel()
+
+        binding.logInButton.setOnClickListener {
+            val email = binding.emailTv.text.toString()
+            val password = binding.passwordTv.text.toString()
+            viewModel.logInUser(email, password)
+        }
+
+
     }
+
+    private fun observeViewModel() {
+        viewModel.state.observe(this) {
+            when (it) {
+                is LogInState.Success -> {
+                    val intent = Intent(this, SurveysActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else -> {//TODO}
+                }
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/mbcapp/presentation/SurveysActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mbcapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mbcapp.R
+
+class SurveysActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_surveys)
+    }
+}
