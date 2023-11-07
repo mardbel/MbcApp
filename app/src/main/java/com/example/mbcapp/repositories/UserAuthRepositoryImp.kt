c94127cdc52b972591f45f50f76a721c720d0649
martin.delbel <martin.delbel@geopagos.com>
2023-11-07 16:24:05 -0300

    fix invalid credentials case

--- app/src/main/java/com/example/mbcapp/repositories/UserAuthRepositoryImp.kt
+++ app/src/main/java/com/example/mbcapp/repositories/UserAuthRepositoryImp.kt
@@ -3,12 +3,14 @@ package com.example.mbcapp.repositories
 import com.example.mbcapp.model.AccessTokenResponse
 import com.example.mbcapp.model.LoginUser
 import com.example.mbcapp.remote.AuthenticationService
+import com.example.mbcapp.utils.SharePreferencesProvider
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
 class UserAuthRepositoryImp @Inject constructor(
     private val authenticationService: AuthenticationService,
+    private val sharePreferencesProvider: SharePreferencesProvider,
 ) : UserAuthRepository {
 
     override suspend fun logIn(
@@ -29,7 +31,8 @@ class UserAuthRepositoryImp @Inject constructor(
                 is IOException -> UserAuthRepository.AuthenticationResult.NetworkError
                 is HttpException -> when (throwable.code()) {
                     400 -> UserAuthRepository.AuthenticationResult.IncorrectPassword
-                    in 401..499 -> UserAuthRepository.AuthenticationResult.BadRequest
+                    in 401..402 -> UserAuthRepository.AuthenticationResult.BadRequest
+                    403 -> UserAuthRepository.AuthenticationResult.InvalidClient
                     in 500..599 -> UserAuthRepository.AuthenticationResult.ApiError
                     else -> UserAuthRepository.AuthenticationResult.GenericError(throwable)
                 }
@@ -39,7 +42,7 @@ class UserAuthRepositoryImp @Inject constructor(
     }
 
     override suspend fun saveAccessToken(token: AccessTokenResponse) {
-        TODO("Not yet implemented")
+        sharePreferencesProvider.saveToken(token)
     }
 
 
